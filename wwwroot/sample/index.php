40c3c64dbd81d63602bdfbb2c5abd52d9ff51303
Rob Nugen <thunderrabbit.2025_jun_30@lemur13.wasabi>
2026-01-10 10:34:52 +0900

    Create sample page (not working yet)

---
+++ wwwroot/sample/index.php
@@ -0,0 +1,22 @@
+<?php
+
+# Must include here because DH runs FastCGI https://www.phind.com/search?cache=zfj8o8igbqvaj8cm91wp1b7k
+# Extract DreamHost project root: /home/username/domain.com
+preg_match('#^(/home/[^/]+/[^/]+)#', __DIR__, $matches);
+include_once $matches[1] . '/prepend.php';
+
+// Sample page - no authentication required, anonymous users can try the visualizer
+$page = new \Template(config: $config);
+$page->setTemplate("layout/welcome_base.tpl.php");
+$page->set("page_title", "Try It - Minecraft Coordinate Visualizer");
+
+// Get the inner content
+$inner_page = new \Template(config: $config);
+$inner_page->setTemplate("index.tpl.php");
+$inner_page->set("username", "Guest");
+$inner_page->set("is_sample_mode", true); // Flag to disable save/load functionality
+$inner_page->set("site_version", SENTIMENTAL_VERSION);
+$page->set("page_content", $inner_page->grabTheGoods());
+
+$page->echoToScreen();
+exit;
